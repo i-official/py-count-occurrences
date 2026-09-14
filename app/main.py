a5bd37799454ea5deae67759b00ae51aac9674ef
i-official <io.yarbess@gmail.com>
2026-09-14 15:15:42 +0300

    remove debug prints

--- app/main.py
+++ app/main.py
@@ -1,6 +1,3 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     return phrase.lower().count(letter.lower())
-print(count_occurrences("letter", "t"))
-print(count_occurrences("abc", "a"))
-print(count_occurrences("abc", "d"))
-print(count_occurrences("ABC", "a"))
+
